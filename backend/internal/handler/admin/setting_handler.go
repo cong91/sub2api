494c0c648e4bab950b345d67a6e098c496a3617a
cong91 <thanhcong191@gmail.com>
2026-05-09 19:30:54 +0700

    fix(admin): return affiliate setting state (#39)

--- backend/internal/handler/admin/setting_handler.go
+++ backend/internal/handler/admin/setting_handler.go
@@ -267,6 +267,7 @@ func (h *SettingHandler) GetSettings(c *gin.Context) {
 		CyberSessionBlockEnabled:                               settings.CyberSessionBlockEnabled,
 		CyberSessionBlockTTLSeconds:                            settings.CyberSessionBlockTTLSeconds,
 		DeviceClaimBonusBalance:                                settings.DeviceClaimBonusBalance,
+		AffiliateEnabled:                                       settings.AffiliateEnabled,
 		DeviceAutoActivationAffCodes:                           settings.DeviceAutoActivationAffCodes,
 		AffiliateRebateRate:                                    settings.AffiliateRebateRate,
 		AffiliateRebateFreezeHours:                             settings.AffiliateRebateFreezeHours,
@@ -392,8 +393,6 @@ func (h *SettingHandler) GetSettings(c *gin.Context) {
 		ModelPlazaEnabled:     settings.ModelPlazaEnabled,
 		ModelPlazaRequireAuth: settings.ModelPlazaRequireAuth,
 		ModelPlazaDescription: settings.ModelPlazaDescription,
-
-		AffiliateEnabled: settings.AffiliateEnabled,
 
 		AccountSchedulingThresholds: settings.AccountSchedulingThresholds,
 		AllowUserViewErrorRequests:  settings.AllowUserViewErrorRequests,
