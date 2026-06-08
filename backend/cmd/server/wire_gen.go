4a0254ecb2419956e3139cce84d12b3b795d8b5e
cong91 <thanhcong191@gmail.com>
2026-06-08 12:48:43 +0700

    fix: use default limits for bot-sales buyers (#93)

--- backend/cmd/server/wire_gen.go
+++ backend/cmd/server/wire_gen.go
@@ -252,7 +252,7 @@ func initializeApplication(buildInfo handler.BuildInfo) (*Application, error) {
 	paymentHandler := admin.NewPaymentHandler(paymentService, paymentConfigService, adminService)
 	affiliateHandler := admin.NewAffiliateHandler(affiliateService, adminService, client)
 	complianceHandler := admin.NewComplianceHandler(settingService)
-	botSalesFulfillmentService := service.NewBotSalesFulfillmentService(client, userService, subscriptionService, apiKeyService, userDeviceRepository)
+	botSalesFulfillmentService := service.NewBotSalesFulfillmentService(client, userService, settingService, subscriptionService, apiKeyService, userDeviceRepository)
 	botSalesFulfillmentHandler := admin.NewBotSalesFulfillmentHandler(botSalesFulfillmentService)
 	adminHandlers := handler.ProvideAdminHandlers(dashboardHandler, adminUserHandler, groupHandler, accountHandler, adminAnnouncementHandler, dataManagementHandler, backupHandler, oAuthHandler, openAIOAuthHandler, geminiOAuthHandler, antigravityOAuthHandler, kiroOAuthHandler, proxyHandler, adminRedeemHandler, promoHandler, settingHandler, opsHandler, systemHandler, adminSubscriptionHandler, adminUsageHandler, userAttributeHandler, errorPassthroughHandler, tlsFingerprintProfileHandler, adminAPIKeyHandler, scheduledTestHandler, channelHandler, channelMonitorHandler, channelMonitorRequestTemplateHandler, contentModerationHandler, paymentHandler, affiliateHandler, complianceHandler, botSalesFulfillmentHandler)
 	usageRecordWorkerPool := service.NewUsageRecordWorkerPool(configConfig)
