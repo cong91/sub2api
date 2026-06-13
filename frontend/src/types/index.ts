ecb1ea22c359a8deaf258d0ac82ab61b7086d5e4
cong91 <thanhcong191@gmail.com>
2026-06-13 15:18:03 +0700

    feat: support multi-group account import (#103)

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1499,6 +1499,7 @@ export interface AdminDataImportProxyAssignment {
 export interface AdminDataImportRequest {
   data: AdminDataPayload
   skip_default_group_bind?: boolean
+  group_ids?: number[]
   group_id?: number | null
   proxy_assignment?: AdminDataImportProxyAssignment
 }
