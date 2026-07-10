04220fd95ccbce3dfad2f797d7c62a9c7ed0914c
mrc <thanhcong191@gmail.com>
2026-07-10 09:22:33 +0700

    fix: reconcile post-rebase CI drift

--- backend/internal/server/routes/gateway_test.go
+++ backend/internal/server/routes/gateway_test.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/Wei-Shaw/sub2api/internal/config"
 	"github.com/Wei-Shaw/sub2api/internal/handler"
+	"github.com/Wei-Shaw/sub2api/internal/pkg/ctxkey"
 	servermiddleware "github.com/Wei-Shaw/sub2api/internal/server/middleware"
 	"github.com/Wei-Shaw/sub2api/internal/service"
 	"github.com/gin-gonic/gin"
@@ -41,10 +43,12 @@ func newGatewayRoutesTestRouterWithConfig(cfg *config.Config, platform ...string
 		},
 		servermiddleware.APIKeyAuthMiddleware(func(c *gin.Context) {
 			groupID := int64(1)
+			group := &service.Group{Platform: groupPlatform}
 			c.Set(string(servermiddleware.ContextKeyAPIKey), &service.APIKey{
 				GroupID: &groupID,
-				Group:   &service.Group{Platform: groupPlatform},
+				Group:   group,
 			})
+			c.Request = c.Request.WithContext(context.WithValue(c.Request.Context(), ctxkey.Group, group))
 			c.Next()
 		}),
 		nil,
