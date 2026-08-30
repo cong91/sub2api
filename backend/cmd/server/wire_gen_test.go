c058ac1b8d27667df6602d69dfcaad033649a61e
mrc2204 <thanhcong191@gmail.com>
2026-08-30 09:40:05 +0700

    test: update wire cleanup fixture for auto provision

--- backend/cmd/server/wire_gen_test.go
+++ backend/cmd/server/wire_gen_test.go
@@ -90,6 +90,7 @@ func TestProvideCleanup_WithMinimalDependencies_NoPanic(t *testing.T) {
 		nil, // paymentOrderExpiry
 		nil, // channelMonitorRunner
 		nil, // channelMonitorV2Aggregator
+		nil, // openAIAutoProvision
 		nil, // quotaFlusher
 		nil, // upstreamBillingProbe
 		nil, // ollamaCloudUsage
